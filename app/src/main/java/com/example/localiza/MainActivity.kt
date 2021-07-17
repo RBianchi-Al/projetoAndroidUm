bf4d460c28ebb656954e53fd7897ac5eecef6848
RBianchi-Al <renatalebianchi@gmail.com>
2021-07-17 13:11:56 -0300

    tela login

--- app/src/main/java/com/example/localiza/MainActivity.kt
+++ app/src/main/java/com/example/localiza/MainActivity.kt
@@ -7,5 +7,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(ehUmBomFilme("Vingadores: Endgame"))
+        criarNumeros()
+
     }
+
+    fun ehUmBomFilme(filme: String?): String{
+       return when{
+           filme.isNullOrEmpty() -> "Erro, preciso de um nome para avaliar"
+           filme.length < 5 -> "um nome tão curto não pode ser bom"
+           else -> "é, talvez seja bom!"
+       }
+    }
+    fun criarNumeros(){
+        val list = List(10){it+1}
+        var mutableList = mutableListOf<String>()
+        list.forEach {
+            mutableList.add(if (it%2 == 0 ) it.toString() else "-")
+        }
+        println(mutableList)
+    }
+
+
 }
---
+++ app/src/test/java/com/example/localiza/main.kt
@@ -0,0 +1,8 @@
+package com.example.localiza
+
+fun main() {
+    
+
+
+}
+
